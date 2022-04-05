7624d0b047dc63c8e937517b2ec8645b4c2e61c1
Rajaji <35359380+raghavraja@users.noreply.github.com>
2022-04-05 22:03:48 +0530

    main

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
-console.log("hello world");
+//conditional opreators
+
+// Ticket System
+
+// above 18 will be consider as a adult;
+// belove 17 will be consider as a children;
+
+let age = 17;
+let ismale = false;
+if(age >= 18 && ismale){
+    console.log("Adult: Male: TIcket Price is Rs:20");
+}
+else if(age >= 18 && ismale == false) {
+console.log("Adult: Female: Ticket Price is Rs: 15");
+}
+else{
+    console.log("Children: Ticket price is Rs:5");
+}
+
+//-------------------START------------------------------------
+// greeting app
+// if hour is between 12am(0) to 13pm (12) --- good morning
+// if hour is between 1pm(13) to 5pm (17) --- good evening
+// if hour is between 5pm(17) to 12am(0) -- good night
+
+let clock = 14;
+if(clock >= 0 && clock <= 13 ){
+console.log("Good Morning");
+}
+else if(clock >=13 && clock<= 17){
+console.log("Good Afternoon");
+}
+else{
+console.log("Good Evening");
+}
+
+// ---------------------END--------------------------------
