073f8cbc20de07f95aa9ef1603e07320988f7b0b
Rajaji <35359380+raghavraja@users.noreply.github.com>
2022-04-05 22:20:37 +0530

    main

--- main.js
+++ main.js
@@ -5,13 +5,11 @@
 
 let age = 17;
 let ismale = false;
-if(age >= 18 && ismale){
+if (age >= 18 && ismale) {
     console.log("Adult: Male: TIcket Price is Rs:20");
-}
-else if(age >= 18 && ismale == false) {
-console.log("Adult: Female: Ticket Price is Rs: 15");
-}
-else{
+} else if (age >= 18 && ismale == false) {
+    console.log("Adult: Female: Ticket Price is Rs: 15");
+} else {
     console.log("Children: Ticket price is Rs:5");
 }
 
@@ -21,17 +19,15 @@ else{
 // if hour is between 1pm(13) to 5pm (17) --- good evening
 // if hour is between 5pm(17) to 12am(0) -- good night
 
-let  mis = new Date();
+let mis = new Date();
 let clock = mis.getHours();
 
-if(clock >= 0 && clock <= 13 ){
-console.log("Good Morning");
-}
-else if(clock >=13 && clock<= 17){
-console.log("Good Afternoon");
-}
-else{
-console.log("Good Evening" + " " + "Time:" + clock);
+if (clock >= 0 && clock <= 13) {
+    console.log("Good Morning");
+} else if (clock >= 13 && clock <= 17) {
+    console.log("Good Afternoon");
+} else {
+    console.log("Good Evening" + " " + "Time:" + clock);
 }
 
 // ---------------------END--------------------------------
